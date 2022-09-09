7cce11ae6d185c3c539b86222f4073d715cab6eb
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-09-09 14:18:21 +0300

    init commit

---
+++ src/main/java/grebnev/yadoa/YadoaApplication.java
@@ -0,0 +1,13 @@
+package grebnev.yadoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YadoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YadoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grebnev/yadoa/YadoaApplicationTests.java
@@ -0,0 +1,13 @@
+package grebnev.yadoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YadoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
