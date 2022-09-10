8a56169a6d2e3f5e941196cfa280e4bb3577b892
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-09-10 09:13:47 +0300

    int

--- src/main/java/grebnev/yadoa/service/SystemItemService.java
+++ src/main/java/grebnev/yadoa/service/SystemItemService.java
@@ -19,40 +19,33 @@ public class SystemItemService {
     private final SystemItemRepository repository;
     private final SystemItemMapper mapper;
 
-    private Map<String, SystemItemEntity> entityToCreate;
+//    private Map<String, SystemItemEntity> entityToCreate;
 //    private Map<String, SystemItemEntity> otherEntitiesToCreate;
-    private Map<String, SystemItemEntity> existingEntities;
-    private Set<Object> idsFromRequest;
+//    private Map<String, SystemItemEntity> existingEntities;
+//    private Set<Object> idsFromRequest;
 
     @Transactional
     public void add(SystemItemImportRequest request) {
-        List<SystemItemEntity> entitiesToAdd = convertRequestToEntities(request);
+        Set<String> parentsIdsFromRequest = getParentsIdsFromRequest(request);
+        List<SystemItemEntity> entitiesToSave = new ArrayList<>();
 
+        for (SystemItemImport dto : request.getItems()) {
+            String curId = dto.getId();
+            String parentId = dto.getParentId();
+            Optional<SystemItemEntity> currentOptionalEntity;
 
-
-
-
-
-//        Set<String> parentsIdsFromRequest = getParentsIdsFromRequest(request);
-//        List<SystemItemEntity> entitiesToSave = new ArrayList<>();
-//
-//        for (SystemItemImport dto : request.getItems()) {
-//            String curId = dto.getId();
-//            String parentId = dto.getParentId();
-//            Optional<SystemItemEntity> currentOptionalEntity;
-//
-//            if (parentId == null || parentsIdsFromRequest.contains(parentId)) {
-//                currentOptionalEntity = repository.findById(curId);
-//            } else {
-//                List<SystemItemEntity> currentOrParent = repository.findAllById(List.of(curId, parentId));
-//                Optional<SystemItemEntity> parentOptionalEntity = getFromListById(currentOrParent, parentId);
-//                if (parentOptionalEntity.isEmpty()) throw new ValidationException("Parent for item isn't exist");
-//                currentOptionalEntity = getFromListById(currentOrParent, curId);
-//            }
-//            entitiesToSave.add(mapper.dtoToEntity(dto, request.getUpdateDate()));
-//        }
-//        repository.saveAll(entitiesToSave);
-        //first revision
+            if (parentId == null || parentsIdsFromRequest.contains(parentId)) {
+                currentOptionalEntity = repository.findById(curId);
+            } else {
+                List<SystemItemEntity> currentOrParent = repository.findAllById(List.of(curId, parentId));
+                Optional<SystemItemEntity> parentOptionalEntity = getFromListById(currentOrParent, parentId);
+                if (parentOptionalEntity.isEmpty()) throw new ValidationException("Parent for item isn't exist");
+                currentOptionalEntity = getFromListById(currentOrParent, curId);
+            }
+            entitiesToSave.add(mapper.dtoToEntity(dto, request.getUpdateDate()));
+        }
+        repository.saveAll(entitiesToSave);
+//        first revision
 //            SystemItemEntity entity;
 //            if (currentOptionalEntity.isPresent()) {
 //                entity = currentOptionalEntity.get();
@@ -65,34 +58,34 @@ public class SystemItemService {
 //        }
     }
 
-    private List<SystemItemEntity> convertRequestToEntities(SystemItemImportRequest request) {
-        List<SystemItemImport> imports = request.getItems();
-        fillMapsToCreate(imports);
-        List<SystemItemEntity> entitiesFromRepo = repository.findAllById(entityToCreate.keySet());
-        for (SystemItemEntity existingEntity : entitiesFromRepo) {
-            String curId = existingEntity.getId();
-            entityToCreate.remove(curId);
-            existingEntities.put(curId, existingEntity);
-        }
-        ArrayList<SystemItemEntity> entitiesToSave = new ArrayList<>(imports.size());
-        fillEntitiesToSave(entitiesToSave, )
-
-    }
-
-    private void fillMapsToCreate(List<SystemItemImport> imports) {
-        entityToCreate = new HashMap<>(imports.size());
-        existingEntities = new HashMap<>(imports.size());
-        idsFromRequest = new HashSet<>(imports.size());
-        for (SystemItemImport item : imports) {
-            String parentId = item.getParentId();
-            String curId = item.getId();
-            if (parentId != null) {
-                entityToCreate.put(parentId, null);
-            }
-            entityToCreate.put(curId, null);
-
-        }
-    }
+//    private List<SystemItemEntity> convertRequestToEntities(SystemItemImportRequest request) {
+//        List<SystemItemImport> imports = request.getItems();
+//        fillMapsToCreate(imports);
+//        List<SystemItemEntity> entitiesFromRepo = repository.findAllById(entityToCreate.keySet());
+//        for (SystemItemEntity existingEntity : entitiesFromRepo) {
+//            String curId = existingEntity.getId();
+//            entityToCreate.remove(curId);
+//            existingEntities.put(curId, existingEntity);
+//        }
+//        ArrayList<SystemItemEntity> entitiesToSave = new ArrayList<>(imports.size());
+//        fillEntitiesToSave(entitiesToSave, )
+////
+////    }
+//
+//    private void fillMapsToCreate(List<SystemItemImport> imports) {
+//        entityToCreate = new HashMap<>(imports.size());
+//        existingEntities = new HashMap<>(imports.size());
+//        idsFromRequest = new HashSet<>(imports.size());
+//        for (SystemItemImport item : imports) {
+//            String parentId = item.getParentId();
+//            String curId = item.getId();
+//            if (parentId != null) {
+//                entityToCreate.put(parentId, null);
+//            }
+//            entityToCreate.put(curId, null);
+//
+//        }
+//    }
 
     private Set<String> getParentsIdsFromRequest(SystemItemImportRequest request) {
         return request.getItems().stream()
